c2b6dd4e62966dffd359a9ff20eafd68672f2a3a
Yolène <yoleneconstable@live.fr>
2026-03-05 22:10:20 +0100

    feat: add hero section

---
+++ components/sections/Hero.tsx
@@ -0,0 +1,18 @@
+const Hero = () => {
+  return (
+    <section className="relative text-primary-text min-h-screen bg-[url(/images/hero-bg.jpg)] bg-cover bg-center flex flex-col justify-end">
+      <div className="image-overlay-dark"></div>
+      <p className="text-xs md:text-sm uppercase tracking-[0.3em] mb-3">
+        Candidate n°1 - Miss Hauts-de-Seine
+      </p>
+      <h1 className="relative z-1">
+        Janisse <span className="block italic">Constable</span>
+      </h1>
+      <p className="tracking-wide">
+        « Avec simplicité, élégance et le cœur de représenter. »
+      </p>
+    </section>
+  );
+};
+
+export default Hero;
