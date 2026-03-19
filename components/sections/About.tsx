40f63a260fb661e9e419b7a8b48110b5b9f24101
Yolène <yoleneconstable@live.fr>
2026-03-19 18:43:49 +0100

    feat: update about text

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -17,39 +17,28 @@ const About = () => {
         <h2>Qui suis-je ?</h2>
         <div className="space-y-5">
           <p>
-            Je m’appelle Janisse, j’ai 25 ans, j’ai grandi dans un environnement
-            multi-culturel, entre origines guyanaises et polonaises et un
-            parcours universitaire en Floride. Je suis diplômée d’un master en
-            droit et en éthique des affaires et je travaille depuis deux ans
-            dans le domaine de la Compliance.
+            J&apos;ai 25 ans et j&apos;ai grandi entre cultures guyanaise et
+            polonaise, avec un parcours universitaire en France et en Floride.
+            Diplômée d&apos;un master en droit et éthique des affaires, je
+            travaille aujourd&apos;hui en Compliance.
           </p>
           <p>
-            Originaire du Val d’Oise, je suis fière d’être francilienne et
-            d’habiter depuis un an dans les Hauts‑de‑Seine, pour
-            l&apos;attractivité économique, culturelle, la qualité de vie, et la
-            proximité avec Paris. Je m’y sens merveilleusement bien.
+            Originaire du Val d&apos;Oise et installée dans les Hauts-de-Seine,
+            je serais fière de représenter une région dynamique, riche par sa
+            diversité et son énergie et où je m&apos;y sens merveilleusement
+            bien.
           </p>
           <p>
-            Même s’il est loin d&apos;être terminé, mon parcours jusqu&apos;à
-            cette candidature représente l’aboutissement d’un long chemin
-            personnel, l’apprentissage de valeurs, la quête de confiance en soi
-            et surtout, de la discipline, de la connaissance de soi-même et du
-            monde qui nous entoure.
+            Ma candidature est le reflet d&apos;un chemin personnel rempli de
+            quêtes de discipline, de confiance en soi et de découverte. Animée
+            par l&apos;envie d&apos;avoir un impact positif, je souhaite avant
+            tout inspirer, partager et m&apos;engager.
           </p>
           <p>
-            Animée par l’altruisme, ce qui me tient à cœur est l’impact que l’on
-            peut avoir sur les autres. J’aspire à vivre une expérience unique à
-            travers la rencontre de nouveaux horizons, de nouvelles personnes,
-            l’engagement, le partage, et la diffusion d’énergies positives
-            capables d’apporter un sourire, même dans les moments les plus
-            simples.
-          </p>
-          <p>
-            Plus qu’une couronne, c’est l’opportunité de servir, de représenter
-            dignement et sincèrement, inspirer, défendre des causes qui nous
-            tiennent à coeur et rendre fiers ceux que je représenterai, tout en
-            restant simplement moi-même et en montrant que l’on peut aller aussi
-            loin que l’on souhaite en restant fidèle à soi-même.
+            Plus qu&apos;un titre, je vois cette aventure comme une opportunité
+            de représenter avec sincérité, défendre des causes qui me tiennent à
+            cœur et montrer qu&apos;on peut aller loin en restant fidèle à
+            soi-même.
           </p>
         </div>
       </div>
