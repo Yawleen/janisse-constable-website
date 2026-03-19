659b81cd835dda47b7e08188a2fe5311ed6218b4
Yolène <yoleneconstable@live.fr>
2026-03-19 18:29:08 +0100

    style: remove signature

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -19,9 +19,6 @@ const Hero = () => {
       <h1 className="relative z-3">
         Janisse <span className="block italic">Constable</span>
       </h1>
-      <p className="tracking-wide z-1">
-        « Avec simplicité, élégance et le cœur de représenter. »
-      </p>
     </section>
   );
 };
