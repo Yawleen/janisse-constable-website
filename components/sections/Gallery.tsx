166ee9e51f7f2c8206e013db8116e28a92c3f6af
Yolène <yoleneconstable@live.fr>
2026-03-20 00:33:25 +0100

    style: temporarily change section background color

--- components/sections/Gallery.tsx
+++ components/sections/Gallery.tsx
@@ -38,7 +38,9 @@ const Gallery = () => {
   }, [api]);
 
   return (
-    <section className="bg-[#ebe6df]">
+    <section
+    // className="bg-[#ebe6df]"
+    >
       <div className="max-w-4xl mx-auto">
         <p className="subtitle">Portfolio</p>
         <h2>
