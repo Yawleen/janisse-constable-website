f4762ab35ba59fb32a881c5e25283a204c220852
Yolène <yoleneconstable@live.fr>
2026-05-19 22:45:09 +0200

    feat: highlight Miss IDF participation in hero section

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -2,20 +2,24 @@ import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <section className="relative py-18 md:py-26 text-primary-text min-h-screen flex flex-col justify-end">
+    <section className="relative text-center md:text-left py-10 md:py-26 text-primary-text min-h-screen flex flex-col justify-end">
       <Image
         src="/images/hero-bg.png"
         fill
         sizes="100vw"
-        alt="Janisse Constable, Miss Hauts-de-Seine 2026"
+        alt="Janisse Constable, Miss Hauts-de-Seine 2026 pour Miss Île-de-France"
         className="z-0 object-cover object-[50%_15%]"
         priority
       />
       <div className="image-overlay-dark"></div>
-      <p className="text-base font-bold uppercase tracking-[0.3em] mb-3 z-2">
+      <p className="font-bold uppercase tracking-[0.4em] mb-3 z-2">
         Miss Hauts-de-Seine 2026
       </p>
-      <h1 className="relative z-2">
+      <p className="opacity-80 uppercase tracking-[0.3em] mb-4 z-2">
+        Candidate n°3 <span className="hidden md:inline">—</span>{' '}
+        <span className="block md:inline">Miss Île-de-France</span>
+      </p>
+      <h1 className="z-2">
         Janisse <span className="block italic">Constable</span>
       </h1>
     </section>
