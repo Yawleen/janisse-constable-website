d6fefe93c0dbdeefa64af3d9f2442cbbc0e12a3c
Yolène <yoleneconstable@live.fr>
2026-05-28 00:21:34 +0200

    style: update previous and next buttons letter spacing in About section

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -4,34 +4,34 @@ import { Fragment, useState, useRef } from 'react';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 import TimelineSeparator from '../TimelineSeparator';
 
+const steps: {
+  year: string;
+  title: string;
+  description: string;
+}[] = [
+  {
+    year: '2000',
+    title: 'Les racines',
+    description:
+      'Je suis née à Pontoise et j’ai grandi à Cergy, dans le Val d’Oise, entre culture francilienne et guyanaise. C’est dans cet environnement que se sont construites mes valeurs, mon parcours scolaire et mon ambition, jusqu’à l’obtention de mon master en Droit et en Éthique des Affaires à l’âge de 24 ans.',
+  },
+  {
+    year: '2025',
+    title: 'Un nouveau départ',
+    description:
+      "J'ai finalisé mes études aux Etats-Unis, en Floride, où j'ai vécu une expérience exceptionnelle riche en rencontres et en moments uniques.\n\nPuis je suis rentrée en France avec l'envie de m'installer près de Paris pour y débuter ma carrière professionnelle. C'est là que je me suis installée dans les Hauts-De-Seine, à Nanterre, avec ma petite sœur, puis j'ai intégré un cabinet de consulting où je suis aujourd'hui consultante spécialisée en éthique des affaires.",
+  },
+  {
+    year: '2026',
+    title: 'Un rêve qui devient réalité',
+    description:
+      "Je me suis vite sentie merveilleusement bien dans les Hauts-De-Seine, et j'ai ressenti l'envie de voir plus loin et de réaliser de nouveaux projets. J'ai donc enfin tenté de concrétiser un rêve longtemps considéré comme inatteignable. J'ai approché le Comité Miss Hauts-de-Seine qui m'a sélectionnée pour le casting et l'élection, qui ont conduit à me nommer Miss Hauts-De-Seine 2026.\n\nMon parcours est le reflet d'un chemin personnel rempli de quêtes de discipline, de confiance et de résilience à travers les épreuves de la jeunesse. Je souhaite avant tout être une Miss engagée avec le cœur, la sincérité et la bienveillance, capable d'aider, d'encourager, de motiver et d'avoir un impact positif sur les personnes que je représente.\n\nCette aventure est l'opportunité d'utiliser un titre à bon escient, là où il y en a besoin. C'est aussi l'occasion de s'améliorer, se surpasser, s'ouvrir à de nouveaux horizons et vous faire rêver à travers cette expérience !",
+  },
+];
+
 const About = () => {
   const timelineRef = useRef<HTMLParagraphElement>(null);
   const [activeIndex, setActiveIndex] = useState(0);
-
-  const steps: {
-    year: string;
-    title: string;
-    description: string;
-  }[] = [
-    {
-      year: '2000',
-      title: 'Les racines',
-      description:
-        'Je suis née à Pontoise et j’ai grandi à Cergy, dans le Val d’Oise, entre culture francilienne et guyanaise. C’est dans cet environnement que se sont construites mes valeurs, mon parcours scolaire et mon ambition, jusqu’à l’obtention de mon master en Droit et en Éthique des Affaires à l’âge de 24 ans.',
-    },
-    {
-      year: '2025',
-      title: 'Un nouveau départ',
-      description:
-        "J'ai finalisé mes études aux Etats-Unis, en Floride, où j'ai vécu une expérience exceptionnelle riche en rencontres et en moments uniques.\n\nPuis je suis rentrée en France avec l'envie de m'installer près de Paris pour y débuter ma carrière professionnelle. C'est là que je me suis installée dans les Hauts-De-Seine, à Nanterre, avec ma petite sœur, puis j'ai intégré un cabinet de consulting où je suis aujourd'hui consultante spécialisée en éthique des affaires.",
-    },
-    {
-      year: '2026',
-      title: 'Un rêve qui devient réalité',
-      description:
-        "Je me suis vite sentie merveilleusement bien dans les Hauts-De-Seine, et j'ai ressenti l'envie de voir plus loin et de réaliser de nouveaux projets. J'ai donc enfin tenté de concrétiser un rêve longtemps considéré comme inatteignable. J'ai approché le Comité Miss Hauts-de-Seine qui m'a sélectionnée pour le casting et l'élection, qui ont conduit à me nommer Miss Hauts-De-Seine 2026.\n\nMon parcours est le reflet d'un chemin personnel rempli de quêtes de discipline, de confiance et de résilience à travers les épreuves de la jeunesse. Je souhaite avant tout être une Miss engagée avec le cœur, la sincérité et la bienveillance, capable d'aider, d'encourager, de motiver et d'avoir un impact positif sur les personnes que je représente.\n\nCette aventure est l'opportunité d'utiliser un titre à bon escient, là où il y en a besoin. C'est aussi l'occasion de s'améliorer, se surpasser, s'ouvrir à de nouveaux horizons et vous faire rêver à travers cette expérience !",
-    },
-  ];
 
   const changeActiveIndex = (index: number) => setActiveIndex(index);
 
@@ -100,7 +100,7 @@ const About = () => {
             aria-label="Étape précédente"
           >
             <ChevronLeft className="w-4 h-4 transition-transform duration-500 group-hover:-translate-x-1" />
-            <span className="text-xs font-bold tracking-[0.3em] uppercase md:text-sm">
+            <span className="text-xs font-bold tracking-wider uppercase md:text-sm">
               Précédent
             </span>
           </button>
@@ -110,7 +110,7 @@ const About = () => {
             className="group flex items-center gap-2 hover:text-primary transition-colors duration-500 cursor-pointer"
             aria-label="Étape suivante"
           >
-            <span className="text-xs font-bold tracking-[0.3em] uppercase md:text-sm">
+            <span className="text-xs font-bold tracking-wider uppercase md:text-sm">
               Suivant
             </span>
             <ChevronRight className="w-4 h-4 transition-transform duration-500 group-hover:translate-x-1" />
