f934c6e4bd797ddd45a4cc36d08042be63e6d461
Yolène <yoleneconstable@live.fr>
2026-09-21 21:08:39 +0200

    chore: remove event, vote and sticky vote bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 import Hero from '@/components/sections/Hero';
 import About from '@/components/sections/About';
 import Testimonials from '@/components/sections/Testimonials';
-import Vote from '@/components/sections/Vote';
-import Event from '@/components/sections/Event';
 import Gallery from '@/components/sections/Gallery';
 import Footer from '@/components/sections/Footer';
-import StickyVoteBar from '@/components/StickyVoteBar';
 import Victory from '@/components/sections/Victory';
 import Partners from '@/components/sections/Partners';
 import Actions from '@/components/sections/Actions';
@@ -17,13 +14,10 @@ export default function Home() {
       <About />
       <Actions />
       <Testimonials />
-      <Vote />
-      <Event />
       <Victory />
       <Gallery />
       <Partners />
       <Footer />
-      <StickyVoteBar />
     </main>
   );
 }
